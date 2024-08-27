169c0e3cef7349d2411846a446a1b68558de2731
MichaelHassan-SigmaLabs <trainee.michael.hassan@sigmalabs.co.uk>
2024-08-27 15:50:52 +0100

    2021 - day1

--- 2021/day1/SonarSweep.py
+++ 2021/day1/SonarSweep.py
@@ -5,12 +5,38 @@ def read_data():
         return f.readlines()
 
 
-def count_increases(depths: list) -> int:
-    pass
+def count_increases_single(depths: list) -> int:
+    count = 0
+    temp = int(depths[0])
+
+    for i in range(1, len(depths)):
+        number = int(depths[i])
+        if temp < number:
+            count += 1
+
+        temp = number
+
+    return count
+
+
+def count_increases_multiple(depths: list) -> int:
+    count = 0
+    temp = int(depths[0]) + int(depths[1]) + int(depths[2])
+
+    for i in range(1, len(depths) - 2):
+        number = int(depths[i]) + int(depths[i+1]) + int(depths[i+2])
+        if temp < number:
+            count += 1
+
+        temp = number
+
+    return count
 
 
 if __name__ == "__main__":
 
     file_contents = read_data()
 
-    print(file_contents)
+    print(count_increases_single(file_contents))
+
+    print(count_increases_multiple(file_contents))
