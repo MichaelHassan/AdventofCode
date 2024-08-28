048b456b786a111a1c6cccbddd3803dac7d632e8
MichaelHassan-SigmaLabs <trainee.michael.hassan@sigmalabs.co.uk>
2024-08-28 13:57:17 +0100

    start to day 2

---
+++ 2021/day2/dive.py
@@ -0,0 +1,26 @@
+
+def read_data():
+    with open('data.txt') as f:
+        return f.readlines()
+
+
+def implement_instructions(contents: list, hp: int, dp: int):
+
+    for instruction in contents:
+        split = instruction.split()
+        direction = split[0]
+        amount = int(split[1])
+
+
+def change_position(direction: str, amount: int, hp: int, dp: int) -> (int, int):
+    pass
+
+
+if __name__ == "__main__":
+
+    hp = 0
+    dp = 0
+
+    file_contents = read_data()
+
+    implement_instructions(file_contents, hp, dp)
